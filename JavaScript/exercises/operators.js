1c7a252fe056cd354dd1a9f9a12fc4b3e17d9bb6
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-03-20 18:56:10 +0200

    Class exercises

---
+++ JavaScript/exercises/operators.js
@@ -0,0 +1,63 @@
+/* Naudodami dviejų narių operatorius:
+  1. Suformuokite skaičių 7 iš skaičių 2 ir 5
+  2. Suformuokite skaičių 2 iš skaičių 7, 6 ir 1
+  3. Suformuokite skaičių 11 iš skaičių 10, 20 ir 9
+  4. Suformuokite skaičių 77 iš skaičių 13, 2 ir 7
+  5. Suformuokite skaičių 97 iš skaičių 5, 2 ir 51
+*/
+
+console.group('Naudodami dviejų narių operatorius:');
+{
+  console.group('1. Suformuokite skaičių 7 iš skaičių 2 ir 5');
+  // code...
+  console.groupEnd();
+
+  console.group('2. Suformuokite skaičių 2 iš skaičių 7, 6 ir 1');
+  // code...
+  console.groupEnd();
+
+  console.group('3. Suformuokite skaičių 11 iš skaičių 10, 20 ir 9');
+  const a = 20 / 10 + 9;
+  console.log(a);
+  // code...
+  console.groupEnd();
+
+  console.group('4. Suformuokite skaičių 77 iš skaičių 13, 2 ir 7');
+  // code...
+  console.groupEnd();
+
+  console.group('5. Suformuokite skaičių 97 iš skaičių 5, 2 ir 51');
+  // code...
+  console.groupEnd();
+}
+console.groupEnd();
+
+
+console.group('Naudodami vieno nario operatorius:');
+{
+  console.group('1. Suformuokite skaičių 7 iš skaičių 2 ir 5');
+  // code...
+  console.groupEnd();
+
+  console.group('2. Suformuokite skaičių 2 iš skaičių 7, 6 ir 1');
+  // code...
+  console.groupEnd();
+
+  console.group('3. Suformuokite skaičių 11 iš skaičių 10, 20 ir 9');
+  let a = 20;
+  a /= 10;
+  a += 9;
+  console.log(a);
+  // code...
+  console.groupEnd();
+
+  console.group('4. Suformuokite skaičių 77 iš skaičių 13, 2 ir 7');
+  // code...
+  console.groupEnd();
+
+  console.group('5. Suformuokite skaičių 97 iš skaičių 5, 2 ir 51');
+  // code...
+  console.groupEnd();
+
+}
+console.groupEnd();
