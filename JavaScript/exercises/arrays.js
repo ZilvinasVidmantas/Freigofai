a7ffbe1dc8fc3ef08073f8a550e35fa07afb4869
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-03 18:09:31 +0300

    homework

--- JavaScript/exercises/arrays.js
+++ JavaScript/exercises/arrays.js
@@ -1,10 +1,10 @@
-let numbers = [46, 1, 4, 78, 9, 5, 1, 4, 5, 5, 50];
-// indexes:     0  1  2   3  4  5  6  7  8  9
-let nouns = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake', 'year'];
-// indexes:     0       1         2         3      4        5       6    
-let helpers = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' ', 'was'];
-// indexes:      0      1      2     3     4    5     6     7
-let adjectives = ['great', 'tasty'];
+const numbers = [46, 1, 4, 78, 9, 5, 1, 4, 5, 5, 50];
+// indexes:      0  1  2   3  4  5  6  7  8  9
+const nouns = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake', 'year'];
+// indexes:       0       1         2         3      4        5       6    
+const helpers = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ' ', 'was'];
+// indexes:        0      1      2     3     4    5     6     7
+const adjectives = ['great', 'tasty'];
 // Print out sentences using words from these arrays:
 console.group('1. my sister is in the lake')
 {
@@ -33,7 +33,7 @@ console.group('4. apple is with my sister');
 }
 console.groupEnd();
 
-console.group('5. Papildyti masyvus žodžiais, ir sukurti dar 3 +-logiškus sakinius.');
+console.group('5. Append your words to the arrays and create your own sentences.');
 {
 
 }
---
+++ JavaScript/homework/arrays.js
@@ -0,0 +1,51 @@
+const numbers = [1, 2, 4, 9, -9, 99, 6, 4, 7, 5, 33, 5];
+const results = [];
+
+console.group('Using numbers in the array<numbers> form new numbers, and add them to array<results>');
+
+// example 
+console.group('Form number 13, and the to the end of the array');
+results[0] = numbers[2] + numbers[3];
+console.groupEnd();
+// ----------------------------------------------------------------------
+console.group('Form number 15 and add it to the end of the array<results>');
+// code ...
+console.groupEnd();
+
+// ----------------------------------------------------------------------
+
+console.group('Form number -15 and add it to the end of the array<results>');
+// code ...
+console.groupEnd();
+
+// ----------------------------------------------------------------------
+
+console.group('Form number -26 and add it to the front of the array<results>');
+// code ...
+console.groupEnd();
+
+// ----------------------------------------------------------------------
+
+console.group('Form number 126 and add it to the front of the array<results>');
+// code ...
+console.groupEnd();
+
+// ----------------------------------------------------------------------
+
+console.group('Form number 111 and add it to the middle of the array<results>');
+// code ...
+console.groupEnd();
+
+// ----------------------------------------------------------------------
+
+console.group('Form number 69 and add it to the middle of the array<results>');
+// code ...
+console.groupEnd();
+
+
+
+
+
+
+console.groupEnd();
+
