12d5ad9404c6428c576dbef23120bd83af18d9df
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-10 18:16:41 +0300

    task: Dialogue with 'if condition'

--- JavaScript/concepts/conditions.js
+++ JavaScript/concepts/conditions.js
@@ -4,18 +4,40 @@ const customerAge = 25;
 const legalSmokingAgeLimit = 18;
 const legalDrinkingAgeLimit = 20;
 
-console.log(`Customer: Hello, I'd like to have some legal narcotics`);
+console.log(`Customer: Hello, I'd like to have some legal narcotics.`);
 console.log('Vendor: Can I see your Id card, please?');
-console.log('Customer: Here you go');
+console.log('Customer: Here you go!');
 console.log(`Vendor: As I see you are ${customerAge} of age ...`);
 if (customerAge >= legalSmokingAgeLimit) {
   console.log('Vendor: What cigarettes do you want to have?')
 } else {
-  console.log('Vendor: Milk section is in another part of the store, young man');
+  console.log('Vendor: Milk section is in another part of the store, young man!');
 }
 
 if (customerAge >= legalDrinkingAgeLimit) {
   console.log('Vendor: What booze you want to buy?');
 } else {
-  console.log('Vendor: Please consider Coca-cola young fellow.')
+  console.log('Vendor: Please consider a Coca-cola can, young fellow.')
+}
+
+/* The first block is called the 'then' block, which refers what to do, if the condition is thuthy
+    'then' block is a necessity, you can not have a condition without the 'then' block.
+  You can add an 'else' block if your application logic requires it. Though it is not required.
+  For example:
+*/
+
+const moneyInPocket = 1.29;
+const costOfChocolateBar = 1.49;
+
+if (moneyInPocket >= costOfChocolateBar) {
+  console.log('Customer: Hello, can I have a Chocolate bar?');
+  console.log(`Vendor: Sure Mate! That would be ${costOfChocolateBar}?`);
+  console.log('Customer: Here you go.');
+  console.log('Vendor: Happy colories!');
+  console.log('Customer: Ha! Thanks. Bye!');
+  console.log('Vendor: Good luck, come again!');
+}
+// Optional
+else {
+  console.log('Damn! I wish a have studied programing when I was young...')
 }
--- JavaScript/exercises/conditions.js
+++ JavaScript/exercises/conditions.js
@@ -1,3 +1,7 @@
-console.group('Write any dialogue, in which at least 2 if-else conditions are used');
+console.group('Write any dialogue, in which at least 2 "if-else conditions" are used');
+// code...
+console.groupEnd();
+
+console.group('Write a short dialogue, in which "if condition" is used');
 // code...
 console.groupEnd();
