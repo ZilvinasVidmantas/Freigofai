86876dc7e722c67024738abd1bc62dd113dde638
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-17 18:16:55 +0300

    Selective conditions: 3, 4 tasks

--- JavaScript/exercises/selectiveConditions.js
+++ JavaScript/exercises/selectiveConditions.js
@@ -9,61 +9,61 @@ console.groupEnd();
 const allApples = [{
   type: 'Red love',
   weight: 80.2
-},{
+}, {
   type: 'Ambrosia',
   weight: 70.2
-},{
+}, {
   type: 'Opal',
   weight: 90.2
-},{
+}, {
   type: 'Golden',
   weight: 85.2
-},{
+}, {
   type: 'Cortland',
   weight: 87.2
-},{
+}, {
   type: 'Red love',
   weight: 88.2
-},{
+}, {
   type: 'Ambrosia',
   weight: 86.2
-},{
+}, {
   type: 'Opal',
   weight: 80.8
-},{
+}, {
   type: 'Golden',
   weight: 60.2
-},{
+}, {
   type: 'Cortland',
   weight: 89.2
-},{
+}, {
   type: 'Red love',
   weight: 81.2
-},{
+}, {
   type: 'Ambrosia',
   weight: 82.2
-},{
+}, {
   type: 'Opal',
   weight: 83.2
-},{
+}, {
   type: 'Golden',
   weight: 80.5
-},{
+}, {
   type: 'Cortland',
   weight: 80.6
-},{
+}, {
   type: 'Red love',
   weight: 80.9
-},{
+}, {
   type: 'Ambrosia',
   weight: 80.8
-},{
+}, {
   type: 'Opal',
   weight: 80.7
-},{
+}, {
   type: 'Golden',
   weight: 80.7
-},{
+}, {
   type: 'Cortland',
   weight: 80.7
 }];
@@ -71,6 +71,17 @@ const allApples = [{
 console.group('2. Create an switch block to place apples in diferent arrays based on their type');
 const redLoveApples = [];
 const ambrosiaApples = [];
+// ... code
+console.groupEnd();
+
+console.group('3. Create an switch block to count their total weight based on their type');
+let redLoveAppleWeight = 0;
+let ambrosiaAppleWeight = 0;
+// ... code
+console.groupEnd();
+
+console.group('4. Create an switch block to calculate average weight of each apple type');
+
 // ... code
 console.groupEnd();
 
