2d12be275b88470b18b591fb912807d8afe4cd95
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-04-24 19:27:06 +0300

    cycles: homework tasks

---
+++ JavaScript/homework/cycles.js
@@ -0,0 +1,61 @@
+const cars = [{
+  brand: 'Opel',
+  model: 'Astra',
+  year: 2005,
+  price: 1300
+},{
+  brand: 'Opel',
+  model: 'Astra',
+  year: 2011,
+  price: 9000
+},{
+  brand: 'Opel',
+  model: 'Zefyra',
+  year: 2010,
+  price: 3300
+},{
+  brand: 'BMW',
+  model: 'X5',
+  year: 2016,
+  price: 26000
+},{
+  brand: 'Subaru',
+  model: 'Impreza',
+  year: 2008,
+  price: 8500
+}];
+
+console.group('1. Print all car brands and models by row');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('2. Sum car prices');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('3. Count average price of all cars');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('4. Count sum of all cars which brand is "Opel"');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('5. Count average of all cars which brand is NOT "Opel"');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('6. Count sum of all cars which are older than 2010');
+// ... code
+console.groupEnd();
+console.log();
+
+console.group('7. Count average of all cars which are newer than 2011 or is of year 2011');
+// ... code
+console.groupEnd();
+console.log();
