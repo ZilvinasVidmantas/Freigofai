461c6d67df322af4cfc58f2a6f46d88f05019622
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-05-01 18:54:44 +0300

    exercises: functions

---
+++ JavaScript/exercises/functions.js
@@ -0,0 +1,84 @@
+const people1 = [{
+  name: 'Sally',
+  surname: 'Jolly',
+  age: 20,
+  weight: 60,
+  height: 172,
+  married: false,
+  sex: 'Woman',
+}, {
+  name: 'John',
+  surname: 'Stevens',
+  age: 67,
+  weight: 80,
+  height: 172,
+  married: true,
+  sex: 'Man',
+}, {
+  name: 'Marrie',
+  surname: 'Alexander',
+  age: 16,
+  weight: 50,
+  height: 157,
+  married: false,
+  sex: 'Woman',
+}];
+
+const people2 = [{
+  name: 'Jonny',
+  surname: 'Lagend',
+  age: 25,
+  weight: 85,
+  height: 183,
+  married: false,
+  sex: 'Man',
+}, {
+  name: 'Jack',
+  surname: 'LeBlanc',
+  age: 36,
+  weight: 90,
+  height: 181,
+  married: true,
+  sex: 'Man',
+}, {
+  name: 'Sofie',
+  surname: 'LeBlanc',
+  age: 38,
+  weight: 71,
+  height: 168,
+  married: true,
+  sex: 'Woman',
+}];
+
+// Please do these exercises and execute them using <people1> and <people2> arrays
+
+console.group('1. Write a function, that takes people array and PRINTS their fullnames');
+// ...code
+console.groupEnd();
+console.log();
+
+console.group('2. Write a function, that takes people array and PRINTS their age average');
+// ...code
+console.groupEnd();
+console.log();
+
+console.group('3. Write a function, that takes people array and PRINTS married people count');
+// ...code
+console.groupEnd();
+console.log();
+
+console.group('4. Write a function, that takes people array and RETUNRS married people count');
+// ...code
+console.groupEnd();
+console.log();
+
+console.group('5. Write a function, that takes people array and RETUNRS children count');
+// ...code
+console.groupEnd();
+console.log();
+
+console.group('6. Write a function, that takes people array and RETUNRS senior count');
+// ...code
+console.groupEnd();
+console.log();
+
