1a514b6f6c0738f825eca44c4ad906808af941c4
Zilwa <zilvinas.vidmantas.93@gmail.com>
2021-05-01 19:30:04 +0300

    homework: functions

---
+++ JavaScript/homework/functions.js
@@ -0,0 +1,107 @@
+// Do these tasks and show their correctness by calling them with different arguments like in example of first task.
+// Every function should be tested with atleast 2 function calls
+//  * One of the function calls must have it's argument(s) passed by value
+//  ** One of the function calls must have it's argument(s) passed bu variable
+console.group("1. Create function 'triple', which multiplies parameter by 3");
+function triple(x) {
+  return x * 3;
+}
+console.group("1.1 triple(7)");
+const res1 = triple(7);
+console.log(res1);
+console.groupEnd();
+console.log();
+
+console.group("1.1 triple(3)");
+const z = 3;
+çonsole.log(triple(z));
+console.groupEnd();
+console.log();
+
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("2. Create function 'square', which returns a square of a number.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("3. Create function 'add', which adds 2 numbers.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("4. Create function 'mul', which multiplies 2 numbers.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("5. Create function 'pow', which raises base number by power (2 parameters).");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("6. Create function 'root', which retuns nth root of a number.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("7. Create function 'squareArea', which return rectangle area.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("8. Create function 'volume', which return volume of a cuboid.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("9. Create function 'arrayDouble' which takes and array and returns new array with values doubled.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("10. Create function 'arrayPower' which takes and array and returns new array width values raised by given power.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("11. Create function which takes an array and print values in this format:  [0] => 64.  ");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("12. Create function which takes an array and returns sum of all elements");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("13. Create function which takes an array and returns average of all elements");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("14. Create function which takes an array and returns bigest value in the array.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
+
+console.group("15. Create function which takes an array and returns smallest value in the array.");
+// ... code
+console.groupEnd();
+console.log();
+// -----------------------------------------------------------------------------------------
